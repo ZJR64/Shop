c173ffcbc98e58d23892f17410910739b9474f71
Zak Rutherford <zjr6302@rit.edu>
2023-02-20 18:27:33 -0500

    Created Test for update

--- estore-api/src/test/java/com/estore/api/estoreapi/EstoreApiApplicationTests.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/EstoreApiApplicationTests.java
@@ -1,13 +1,48 @@
 package com.estore.api.estoreapi;
 
+import java.beans.Transient;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.estore.api.estoreapi.model.Product;
+import com.estore.api.estoreapi.persistence.ProductFileDAO;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EstoreApiApplicationTests {
+	ProductFileDAO productFileDAO;
+    Product[] testProducts;
+    ObjectMapper mockObjectMapper;
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+    public void testUpdateHero() {
+        // Setup
+        
+		Product product = new Product(99,"Straight Milk", "Coffee");
+
+        // Invoke
+        Product result = assertDoesNotThrow(() -> productFileDAO.updateProduct(product),
+                                "Unexpected exception thrown");
+
+        // Analyze
+        assertNotNull(result);
+        Product actual = productFileDAO.getProduct(product.getId());
+        assertEquals(actual,product);
+    }
 
 }
