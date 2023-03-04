ca412665c0014174c46880d8dae929b9d67c5371
frankandes <ffa7120@g.rit.edu>
2023-03-04 16:25:19 -0500

    Made code not broken

--- estore-api/src/test/java/com/estore/api/estoreapi/EstoreApiUserFileDAOTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/EstoreApiUserFileDAOTest.java
@@ -1,7 +1,12 @@
+package com.estore.api.estoreapi;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +15,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import com.estore.api.estoreapi.model.User;
+import com.estore.api.estoreapi.persistence.UserFileDAO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@SpringBootTest
+/**
+    * The unit test suite for the UserDAO interface class
+    * This may or may not work
+    * @author FRANK ANDES (ffa7120)
+*/
+
 @Tag("Persistence-tier")
+@SpringBootTest
 @TestPropertySource(locations="classpath:application-test.properties")
-public class UserFileDAOTest {
+public class EstoreApiUserFileDAOTest {
 
     @Autowired
     private UserFileDAO userFileDAO;
@@ -30,13 +42,12 @@ public class UserFileDAOTest {
     void setUp() throws IOException {
         // Delete the users file before each test
         assertTrue(new File(usersFilename).delete());
-        userFileDAO.load();
     }
 
     @Test
-    void testAddUser() {
-        User user = new User(0, "John Doe");
-        userFileDAO.addUser(user);
+    void testAddUser() throws IOException {
+        User user = new User(0, "JohnDoe@example.com", "John Doe", "password123", "123 Main Street SW", null, null);
+        userFileDAO.createUser(user);
         User[] users = userFileDAO.getUsers();
         assertEquals(1, users.length);
         assertEquals(user, users[0]);
@@ -44,16 +55,16 @@ public class UserFileDAOTest {
 
     @Test
     void testAddUserWithEmptyName() {
-        User user = new User(0, "");
-        assertThrows(IllegalArgumentException.class, () -> userFileDAO.addUser(user));
+        User user = new User(0, "JaneDoe@example.com", "Jane doe", "password123", "123 Main Street SW", null, null);
+        assertThrows(IllegalArgumentException.class, () -> userFileDAO.createUser(user));
     }
 
 
     @Test
-    void testUpdateUser() {
-        User user = new User(0, "John Doe");
-        userFileDAO.addUser(user);
-        User updatedUser = new User(user.getId(), "Jane Doe");
+    void testUpdateUser() throws IOException {
+        User user = new User(0, "JohnDoe@example.com", "John Doe", "password123", "123 Main Street SW", null, null);
+        userFileDAO.createUser(user);
+        User updatedUser = new User(user.getId(), "JohnDoe@example.com", "Jane Doe", "otherpassword", "123 Main Street SW", null, null);
         userFileDAO.updateUser(updatedUser);
         User[] users = userFileDAO.getUsers();
         assertEquals(1, users.length);
@@ -61,20 +72,20 @@ public class UserFileDAOTest {
     }
 
     @Test
-    void testDeleteUser() {
-        User user = new User(0, "John Doe");
-        userFileDAO.addUser(user);
+    void testDeleteUser() throws IOException {
+        User user = new User(0, "JohnDoe@example.com", "John Doe", "password123", "123 Main Street SW", null, null);
+        userFileDAO.createUser(user);
         userFileDAO.deleteUser(user.getId());
         User[] users = userFileDAO.getUsers();
         assertEquals(0, users.length);
     }
 
     @Test
-    void testGetUsers() {
-        User user1 = new User(0, "John Doe");
-        User user2 = new User(0, "Jane Doe");
-        userFileDAO.addUser(user1);
-        userFileDAO.addUser(user2);
+    void testGetUsers() throws IOException {
+        User user1 = new User(0, "JohnDoe@example.com", "John Doe", "password123", "123 Main Street SW", null, null);
+        User user2 = new User(1, "JaneDoe@example.com", "Jane doe", "password123", "123 Main Street SW", null, null);
+        userFileDAO.createUser(user1);
+        userFileDAO.createUser(user2);
         User[] users = userFileDAO.getUsers();
         assertEquals(2, users.length);
         assertTrue(Arrays.asList(users).contains(user1));
@@ -82,35 +93,14 @@ public class UserFileDAOTest {
     }
 
     @Test
-    void testGetUsersWithFilter() {
-        User user1 = new User(0, "John Doe");
-        User user2 = new User(0, "Jane Doe");
-        userFileDAO.addUser(user1);
-        userFileDAO.addUser(user2);
-        User[] users = userFileDAO.getUsers("Jane");
+    void testGetUsersWithFilter() throws IOException {
+        User user1 = new User(0, "JohnDoe@example.com", "John Doe", "password123", "123 Main Street SW", null, null);
+        User user2 = new User(1, "JaneDoe@example.com", "Jane doe", "password123", "123 Main Street SW", null, null);
+        userFileDAO.createUser(user1);
+        userFileDAO.createUser(user2);
+        User[] users = userFileDAO.findUsers("Jane Doe");
         assertEquals(1, users.length);
         assertTrue(Arrays.asList(users).contains(user2));
         assertFalse(Arrays.asList(users).contains(user1));
-    }
-
-    @Test
-    void testLoad() throws IOException {
-        User user = new User(0, "John Doe");
-        userFileDAO.addUser(user);
-        userFileDAO.save();
-        UserFileDAO userFileDAO2 = new UserFileDAO(usersFilename, objectMapper);
-        User[] users = userFileDAO2.getUsers();
-        assertEquals(1, users.length);
-        assertEquals(user, users[0]);
-    }
-
-    @Test
-    void testSave() throws IOException {
-        User user = new User(0, "John Doe");
-        userFileDAO.addUser(user);
-        userFileDAO.save();
-        User[] users = objectMapper.readValue(new File(usersFilename), User[].class);
-        assertEquals(1,users.length);
-        assertEquals(user, users[0]);
     }
 }
