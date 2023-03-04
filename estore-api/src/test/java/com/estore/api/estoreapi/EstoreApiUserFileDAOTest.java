4c19080f9163cef5f2faef277da99cf867ef92be
frankandes <ffa7120@g.rit.edu>
2023-03-04 17:33:51 -0500

    frank tests done yippee

--- estore-api/src/test/java/com/estore/api/estoreapi/EstoreApiUserFileDAOTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/EstoreApiUserFileDAOTest.java
@@ -1,106 +1,209 @@
 package com.estore.api.estoreapi;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 import com.estore.api.estoreapi.model.User;
 import com.estore.api.estoreapi.persistence.UserFileDAO;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
 /**
-    * The unit test suite for the UserDAO interface class
-    * This may or may not work
-    * @author FRANK ANDES (ffa7120)
-*/
-
+ * Test the Hero File DAO class
+ * 
+ * @author SWEN Faculty
+ */
 @Tag("Persistence-tier")
-@SpringBootTest
-@TestPropertySource(locations="classpath:application-test.properties")
 public class EstoreApiUserFileDAOTest {
+    UserFileDAO userFileDAO;
+    User[] testUsers;
+    ObjectMapper mockObjectMapper;
 
-    @Autowired
-    private UserFileDAO userFileDAO;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Value("${users.file}")
-    private String usersFilename;
-
+    /**
+     * Before each test, we will create and inject a Mock Object Mapper to
+     * isolate the tests from the underlying file
+     * @throws IOException
+     */
     @BeforeEach
-    void setUp() throws IOException {
-        // Delete the users file before each test
-        assertTrue(new File(usersFilename).delete());
+    public void setupUserFileDAO() throws IOException {
+        mockObjectMapper = mock(ObjectMapper.class);
+        testUsers = new User[3];
+        String[] pay = {"ROLE_USER"};
+        testUsers[0] = new User(99,"JaneDoe@example.com", "Jane Doe", "password123", "123 Main Street", false, pay);
+        testUsers[1] = new User(100,"JohnDoe@example.com", "John Doe", "password123", "1234 Main Street", false, pay);
+        testUsers[2] = new User(101,"Booty@example.com", "Ja Booty", "password123", "123 Jabooty Street", false, pay);
+
+        // When the object mapper is supposed to read from the file
+        // the mock object mapper will return the user array above
+        when(mockObjectMapper
+            .readValue(new File("doesnt_matter.txt"),User[].class))
+                .thenReturn(testUsers);
+            
+                userFileDAO = new UserFileDAO("doesnt_matter.txt", mockObjectMapper);
+        
     }
 
     @Test
-    void testAddUser() throws IOException {
-        User user = new User(0, "JohnDoe@example.com", "John Doe", "password123", "123 Main Street SW", null, null);
-        userFileDAO.createUser(user);
+    public void testGetUsers() {
+        // Invoke
         User[] users = userFileDAO.getUsers();
-        assertEquals(1, users.length);
-        assertEquals(user, users[0]);
+
+        // Analyze
+        assertEquals(users.length,testUsers.length);
+        for (int i = 0; i < testUsers.length;++i)
+            assertEquals(users[i],testUsers[i]);
     }
 
     @Test
-    void testAddUserWithEmptyName() {
-        User user = new User(0, "JaneDoe@example.com", "Jane doe", "password123", "123 Main Street SW", null, null);
-        assertThrows(IllegalArgumentException.class, () -> userFileDAO.createUser(user));
-    }
+    public void testFindUsers() {
+        // Invoke
+        User[] users = userFileDAO.findUsers("Doe");
 
-
-    @Test
-    void testUpdateUser() throws IOException {
-        User user = new User(0, "JohnDoe@example.com", "John Doe", "password123", "123 Main Street SW", null, null);
-        userFileDAO.createUser(user);
-        User updatedUser = new User(user.getId(), "JohnDoe@example.com", "Jane Doe", "otherpassword", "123 Main Street SW", null, null);
-        userFileDAO.updateUser(updatedUser);
-        User[] users = userFileDAO.getUsers();
-        assertEquals(1, users.length);
-        assertEquals(updatedUser, users[0]);
+        // Analyze
+        assertEquals(users.length,2);
+        assertEquals(users[0],testUsers[0]);
+        assertEquals(users[1],testUsers[1]);
     }
 
     @Test
-    void testDeleteUser() throws IOException {
-        User user = new User(0, "JohnDoe@example.com", "John Doe", "password123", "123 Main Street SW", null, null);
-        userFileDAO.createUser(user);
-        userFileDAO.deleteUser(user.getId());
-        User[] users = userFileDAO.getUsers();
-        assertEquals(0, users.length);
+    public void testGetHero() {
+        // Invoke
+        User hero = userFileDAO.getUser(99);
+
+        // Analzye
+        assertEquals(hero,testUsers[0]);
     }
 
     @Test
-    void testGetUsers() throws IOException {
-        User user1 = new User(0, "JohnDoe@example.com", "John Doe", "password123", "123 Main Street SW", null, null);
-        User user2 = new User(1, "JaneDoe@example.com", "Jane doe", "password123", "123 Main Street SW", null, null);
-        userFileDAO.createUser(user1);
-        userFileDAO.createUser(user2);
-        User[] users = userFileDAO.getUsers();
-        assertEquals(2, users.length);
-        assertTrue(Arrays.asList(users).contains(user1));
-        assertTrue(Arrays.asList(users).contains(user2));
+    public void testDeleteHero() {
+        // Invoke
+        boolean result = assertDoesNotThrow(() -> userFileDAO.deleteUser(99),
+                            "Unexpected exception thrown");
+
+        // Analzye
+        assertEquals(result,true);
+        // We check the internal tree map size against the length
+        // of the test users array - 1 (because of the delete)
+        // Because User attribute of HeroFileDAO is package private
+        // we can access it directly
+        assertEquals(userFileDAO.users.size(),testUsers.length-1);
     }
 
     @Test
-    void testGetUsersWithFilter() throws IOException {
-        User user1 = new User(0, "JohnDoe@example.com", "John Doe", "password123", "123 Main Street SW", null, null);
-        User user2 = new User(1, "JaneDoe@example.com", "Jane doe", "password123", "123 Main Street SW", null, null);
-        userFileDAO.createUser(user1);
-        userFileDAO.createUser(user2);
-        User[] users = userFileDAO.findUsers("Jane Doe");
-        assertEquals(1, users.length);
-        assertTrue(Arrays.asList(users).contains(user2));
-        assertFalse(Arrays.asList(users).contains(user1));
+    public void testCreateHero() {
+        // Setup
+        // make array of strings
+        String[] pay = {"ROLE_USER"};
+
+        User user = new User(98,"Clap@example.com", "Ja Booty Clap", "password123", "123 Jabooty Street", false, pay);
+
+        // Invoke
+        User result = assertDoesNotThrow(() -> userFileDAO.createUser(user),
+                                "Unexpected exception thrown");
+
+        // Analyze
+        assertNotNull(result);
+        User actual = user;
+        assertEquals(actual.getId(),user.getId());
+        assertEquals(actual.getName(),user.getName());
+    }
+
+    @Test
+    public void testUpdateHero() {
+        // Setup
+
+        String[] pay = {"ROLE_USER"};
+        User user = new User(99,"Za@example.com", "Za Clap", "password123", "123 Jabooty Street", false, pay);
+
+        // Invoke
+        User result = assertDoesNotThrow(() -> userFileDAO.updateUser(user),
+                                "Unexpected exception thrown");
+
+        // Analyze
+        assertNotNull(result);
+        User actual = userFileDAO.getUser(user.getId());
+        assertEquals(actual,user);
+    }
+
+    @Test
+    public void testSaveException() throws IOException{
+        String[] pay = {"ROLE_USER"};
+
+        doThrow(new IOException())
+            .when(mockObjectMapper)
+                .writeValue(any(File.class),any(User[].class));
+
+            
+        User user = new User(102,"RAAAAAA@example.com", "Dino", "password123", "123 Jabooty Street", false, pay);
+
+        assertThrows(IOException.class,
+                        () -> userFileDAO.createUser(user),
+                        "IOException not thrown");
+    }
+
+    @Test
+    public void testGetHeroNotFound() {
+        // Invoke
+        User user = userFileDAO.getUser(98);
+
+        // Analyze
+        assertEquals(user,null);
+    }
+
+    @Test
+    public void testDeleteHeroNotFound() {
+        // Invoke
+        boolean result = assertDoesNotThrow(() -> userFileDAO.deleteUser(98),
+                                                "Unexpected exception thrown");
+
+        // Analyze
+        assertEquals(result,false);
+        assertEquals(userFileDAO.users.size(),testUsers.length);
+    }
+
+    @Test
+    public void testUpdateHeroNotFound() {
+        // Setup
+
+        String[] pay = {"ROLE_USER"};
+        User user = new User(98,"RadioHead@example.com", "Saul Goodman", "password123", "123 Jabooty Street", false, pay);
+
+        // Invoke
+        User result = assertDoesNotThrow(() -> userFileDAO.updateUser(user), "Unexpected exception thrown");
+
+        // Analyze
+        assertNull(result);
+    }
+
+    @Test
+    public void testConstructorException() throws IOException {
+        // Setup
+        ObjectMapper mockObjectMapper = mock(ObjectMapper.class);
+        // We want to simulate with a Mock Object Mapper that an
+        // exception was raised during JSON object deseerialization
+        // into Java objects
+        // When the Mock Object Mapper readValue method is called
+        // from the HeroFileDAO load method, an IOException is
+        // raised
+        doThrow(new IOException())
+            .when(mockObjectMapper)
+                .readValue(new File("doesnt_matter.txt"),User[].class);
+
+        // Invoke & Analyze
+        assertThrows(IOException.class,
+                        () -> new UserFileDAO("doesnt_matter.txt",mockObjectMapper),
+                        "IOException not thrown");
     }
 }
