1827a9b47a6ac7ec5f39cdc30820b933d938f09a
Zak Rutherford <zjr6302@rit.edu>
2023-03-05 16:39:17 -0500

    created login

--- estore-ui/estore-ui/src/app/app.module.ts
+++ estore-ui/estore-ui/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    UsersComponent
+    UsersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
