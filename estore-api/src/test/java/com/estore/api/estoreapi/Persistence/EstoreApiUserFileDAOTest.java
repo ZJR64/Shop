c77faf10cfa292df9de98072ab9881b47b9098b3
Zak Rutherford <zjr6302@rit.edu>
2023-03-04 19:04:12 -0500

    Quick Fixes

--- estore-api/src/test/java/com/estore/api/estoreapi/Persistence/EstoreApiUserFileDAOTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/Persistence/EstoreApiUserFileDAOTest.java
@@ -1,4 +1,4 @@
-package com.estore.api.estoreapi;
+package com.estore.api.estoreapi.Persistence;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
