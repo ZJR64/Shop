10f4eac65049c35e9d6046627314f76dd4a90b68
Zak Rutherford <zjr6302@rit.edu>
2023-03-05 22:37:29 -0500

    Added AuthGaurd

--- estore-ui/estore-ui/src/app/app-routing.module.ts
+++ estore-ui/estore-ui/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuardService } from './services/auth-gaurd.service';
+
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { HomeComponent } from './home/home.component';
@@ -8,13 +10,10 @@ import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuardService]},
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'logout', component: LogoutComponent },
-
-    // otherwise redirect to home
-    { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
--- estore-ui/estore-ui/src/app/app.component.ts
+++ estore-ui/estore-ui/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError} from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Coffee/Tea E-store';
   currentUser = localStorage.getItem('currentUser');
+  currentRoute: string;
+  constructor(private router: Router) {
+    this.currentRoute = "";
+    this.router.events.subscribe((event: Event) => {
+
+        if (event instanceof NavigationEnd) {
+            this.currentRoute = event.url;
+              console.log(event);
+        }
+
+
+    });
+  }
 }
