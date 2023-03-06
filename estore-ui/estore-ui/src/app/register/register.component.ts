d38d134f63c355a2245ff453b24e82d2dfd13a90
Zak Rutherford <zjr6302@rit.edu>
2023-03-05 23:44:50 -0500

    Register Kinda Works

--- estore-ui/estore-ui/src/app/register/register.component.ts
+++ estore-ui/estore-ui/src/app/register/register.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { User } from '../user';
+import { UserService } from '../services/user.service';
 
 @Component({
-  selector: 'app-register',
+  selector: 'app-login',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  users: User[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+    if (localStorage.getItem('currentUser') != null) {
+
+    }
+  }
+
+  getUsers(): void {
+    this.userService.getUsers()
+      .subscribe(users => this.users = users);
+  }
+
+  createNewUser(email: string, name: string, password: string): void {
+    email = email.trim();
+    name = name.trim();
+    password = password.trim();
+    if (!email) { return; }
+    if (!name) {return; }
+    if (!password) { return; }
+
+    this.users.forEach(element => {
+      if (element.email == email) {
+        return;
+      }
+    });
+    
+    this.userService.addUser({email, name, password} as User);
+    this.back();
+  }
+
+  back(): void {
+    this.router.navigateByUrl('/login')
+  }
 
 }
