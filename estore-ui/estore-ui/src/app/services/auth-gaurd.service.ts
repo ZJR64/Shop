fa17db7733ac3c9b80581177f827813be3415fba
Zak Rutherford <zjr6302@rit.edu>
2023-03-07 06:12:35 -0500

    fixed auth-gaurd

--- estore-ui/estore-ui/src/app/services/auth-gaurd.service.ts
+++ estore-ui/estore-ui/src/app/services/auth-gaurd.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router, RouterStateSnapshot, ActivatedRouteSnapshot} from '@angular/router';
+import { map, Observable } from 'rxjs';
 import { LoginService } from './login.service';
 
 @Injectable({ providedIn: 'root' })
@@ -10,15 +11,17 @@ export class AuthGuardService {
     private loginService: LoginService,
   ) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    if (this.loginService.isLoggedIn$) {
-      return true;
-    } else {
-      alert('Please log in')
-      this.router.navigate(['/login']);
-      return false;
-    }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.loginService.isLoggedIn$.pipe(
+      map(isLoggedIn => {
+        if (isLoggedIn) {
+          return true;
+        } else {
+          this.router.navigate(['/login']);
+          return false;
+        }
+      })
+    );
   }
-
-
+  
 }
