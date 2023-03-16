723f8e361f6801760b4afc3b578d5c9320ef7c14
Aadith C <apc3277@g.rit.edu>
2023-03-15 20:43:49 -0400

    Cart works but is showing ID's instead of product names which the customer won't understand :(

--- estore-ui/estore-ui/src/app/cart/cart.component.ts
+++ estore-ui/estore-ui/src/app/cart/cart.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { User } from '../user';
+import { Product } from '../product';
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { UserService } from '../services/user.service';
+import { ProductService } from '../product.service';
+
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +14,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+  user!: User;
+  product!: Product;
 
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private productService: ProductService,
+    private location: Location
+  ) {
+
+    this.userService.getUserFromEmail(localStorage.getItem('currentUser')!).subscribe(
+      (user) => {
+        this.user = user;
+      });
+  }
+
+  ngOnInit() {
+    console.log("opened cart")
+  }
+
+  findProduct(id: number): String {
+    this.getProduct(id);
+    return this.product.name;
+  }
+
+  getProduct(id: number): void {
+    this.productService.getProduct(id)
+      .subscribe(product => this.product = product);
+  }
 }
--- estore-ui/estore-ui/src/app/user.ts
+++ estore-ui/estore-ui/src/app/user.ts
@@ -1,9 +1,10 @@
 export interface User {
-    id: number;
-    email: string;
-    name: string;
-    password : string;
-    address : string;
-    admin : boolean;
-    payInfo : string[];
-  }
+  id: number;
+  email: string;
+  name: string;
+  password: string;
+  address: string;
+  admin: boolean;
+  payInfo: string[];
+  cart: Map<number, number[]>;
+}
