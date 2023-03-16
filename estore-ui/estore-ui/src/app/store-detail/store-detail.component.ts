a73a85e5ea9baf1df1c7bc9df09e09e563b87a4f
Zachary Rutherford <zjr6302@rit.edu>
2023-03-16 19:43:26 -0400

    implemented details

--- estore-ui/estore-ui/src/app/store-detail/store-detail.component.ts
+++ estore-ui/estore-ui/src/app/store-detail/store-detail.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Ingredient } from '../ingredient';
+import { IngredientService } from '../services/ingredients.service';
+
+import { Product } from '../product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-store-detail',
@@ -6,5 +13,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./store-detail.component.css']
 })
 export class StoreDetailComponent {
+  product!: Product;
+  price!: number;
+  size!: number;
+  ingredients!: Ingredient[];
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private ingredientService: IngredientService,
+    ) {
+    const id = +this.route.snapshot.paramMap.get('id')!;
+
+    this.productService.getProduct(id).subscribe(
+      (product: Product) => {
+        this.product = product;
+      });
+     }
+
+  ngOnInit() {
+    this.getIngredients();
+    this.changeSize(10);
+    this.calcPrice();
+  }
+
+  getIngredients(): void {
+    this.ingredientService.getIngredients()
+      .subscribe((ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+        this.calcPrice();}
+      );
+  }
+
+  calcPrice(): void {
+    if (!this.product) {
+      return;
+    }
+    let price: number = 0;
+    const keysArray = Object.keys(this.product.ingredients);
+    const valuesArray = Object.values(this.product.ingredients);
+    var i = -1;
+    keysArray.forEach((name: String) => {
+      i++;
+      this.ingredients.forEach((ingredient: Ingredient) => {
+        if (name == ingredient.name) {
+          price += ingredient.price * valuesArray[i]; 
+        }
+      })
+    });
+  
+    price = price * (1 + this.product.modPrice);
+    price = price * this.size;
+  
+  
+    this.price = parseFloat(price.toFixed(2));;
+  }
+
+  changeSize(size: number) {
+    this.size = size;
+    this.calcPrice();
+  }
+
+  addToCart(): void {
+    //TODO
+    //
+    //
+    //
+    //
+    this.router.navigateByUrl('store');
+  }
 
 }
